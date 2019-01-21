8e90d1224f067ef2afa633d49eb40e26f88b4036
Travis Illig <tillig@paraesthesia.com>
2019-01-21 13:33:52 -0800

    Added rainbow scene.

---
+++ src/PiLights/Scenes/Rainbow.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.ComponentModel;
+using System.Linq;
+using HandlebarsDotNet;
+
+namespace PiLights.Scenes
+{
+    [DisplayName(nameof(Rainbow))]
+    public class Rainbow : Scene
+    {
+        private const string Template = @"rainbow 1
+render{{# if chaseSpeed }}
+do
+  delay {{chaseSpeed}}
+  rotate
+  render
+loop{{/if}}";
+
+        private static readonly Func<object, string> CompiledTemplate = Handlebars.Compile(Template);
+
+        [DisplayName("Chase Speed (0 for no chase)")]
+        public int ChaseSpeed { get; set; }
+
+        public override string GetSceneImplementation()
+        {
+            var data = new
+            {
+                chaseSpeed = this.ChaseSpeed,
+            };
+
+            var generated = CompiledTemplate(data);
+            return generated;
+        }
+    }
+}
