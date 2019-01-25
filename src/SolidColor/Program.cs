87bae54ab110833fb55d370258cd96f115b934ad
Travis Illig <tillig@paraesthesia.com>
2019-01-25 09:52:50 -0800

    SolidColor console app for testing direct integration with WS281x.

---
+++ src/SolidColor/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace SolidColor
+{
+    /// <summary>
+    /// Simple console app to test manual integration with the WS281x library.
+    /// </summary>
+    public class Program
+    {
+        private static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
