28288525c71ed95ff007aa5f0584af4f1e0a6e72
Travis Illig <tillig@paraesthesia.com>
2019-02-09 14:42:16 -0800

    Added new "Breathing" lights scene.

---
+++ src/PiLights/Scenes/Breathing.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.ComponentModel;
+using System.Linq;
+using HandlebarsDotNet;
+using PiLights.Services;
+
+namespace PiLights.Scenes
+{
+    [DisplayName(nameof(Breathing))]
+    public class Breathing : Scene
+    {
+        private const string Template = @"fill 1,{{color}}
+render
+do
+  fade 1,{{defaultBrightness}},0,{{breathSpeed}},1
+  render
+  fade 1,0,{{defaultBrightness}},{{breathSpeed}},1
+  render
+loop";
+
+        private static readonly Func<object, string> CompiledTemplate = Handlebars.Compile(Template);
+
+        [DisplayName("Breath Speed")]
+        [Max(1000)]
+        public int BreathSpeed { get; set; }
+
+        [DisplayName(nameof(Color))]
+        [TypeConverter(typeof(HexColorConverter))]
+        public string Color { get; set; }
+
+        public override string GetSceneImplementation()
+        {
+            var data = new
+            {
+                color = this.Color,
+                breathSpeed = this.BreathSpeed,
+                defaultBrightness = ConfigurationManager.Configuration.GlobalBrightness,
+            };
+
+            var generated = CompiledTemplate(data);
+            return generated;
+        }
+    }
+}
