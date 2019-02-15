9b648778f09f3cb7ae2685d79440ba83b9f7a520
Travis Illig <tillig@paraesthesia.com>
2019-02-15 15:07:57 -0800

    Forms all using Bootstrap 4 and HTML5.

--- src/PiLights/Models/GlobalConfigurationSettings.cs
+++ src/PiLights/Models/GlobalConfigurationSettings.cs
@@ -1,18 +1,34 @@
 ﻿using System;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 
 namespace PiLights.Models
 {
     public class GlobalConfigurationSettings
     {
+        [DisplayName("Global Brightness")]
+        [Range(1, 255)]
+        [Required]
         public int GlobalBrightness { get; set; }
 
+        [DisplayName("LED Count")]
+        [Range(1, int.MaxValue)]
+        [Required]
         public int LedCount { get; set; }
 
+        [DisplayName("LED Type")]
+        [Required]
+        [DataType("Enum")]
         public LedType LedType { get; set; }
 
+        [DisplayName("Server IP Address")]
+        [Required]
         public string ServerIP { get; set; }
 
+        [DisplayName("Server Port")]
+        [Range(1, 32767)]
+        [Required]
         public int ServerPort { get; set; }
     }
 }
