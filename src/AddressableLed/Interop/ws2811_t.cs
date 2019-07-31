3bcfa369c9b50d6f9dc070cfd7e121ce59701a73
Travis Illig <tillig@paraesthesia.com>
2019-07-31 14:27:29 -0700

    Updated .NET Core LED control lib with stuff I resolved from GitHub.

--- src/AddressableLed/Interop/ws2811_t.cs
+++ src/AddressableLed/Interop/ws2811_t.cs
@@ -7,8 +7,9 @@ namespace AddressableLed.Interop
     [SuppressMessage("SA1300", "SA1300", Justification = "Native methods have different naming conventions.")]
     [SuppressMessage("SA1307", "SA1307", Justification = "Native methods have different naming conventions.")]
     [SuppressMessage("SA1310", "SA1310", Justification = "Native methods have different naming conventions.")]
+    [SuppressMessage("SA1401", "SA1401", Justification = "Using a class with public fields instead of a struct to allow memory pinning, but keeping the data simple so fields instead of props.")]
     [StructLayout(LayoutKind.Sequential)]
-    internal struct ws2811_t
+    internal class ws2811_t
     {
         public long render_wait_time;
         public IntPtr device;
@@ -16,5 +17,6 @@ namespace AddressableLed.Interop
         public uint freq;
         public int dmanum;
         public ws2811_channel_t channel_1;
+        public ws2811_channel_t channel_2;
     }
 }
--- src/ConsoleDemo/Program.cs
+++ src/ConsoleDemo/Program.cs
@@ -13,9 +13,6 @@ namespace ConsoleDemo
     {
         private static void Main()
         {
-            // AddressableLed is a fork/combo of...
-            // https://github.com/rpi-ws281x/rpi-ws281x-csharp
-            // https://github.com/chris579/rpi_ws281x.Net
             var settings = new Settings(new Channel(540, StripType.WS2811_STRIP_GRB, brightness: 64));
 
             // using (ILedController controller = new StubLedController(settings))
@@ -29,12 +26,6 @@ namespace ConsoleDemo
                     new RainbowColorAnimation(controller),
                 };
 
-                // TODO: Segmentation fault happens if you try to do this fancy demo.
-                // Working through it here https://github.com/rpi-ws281x/rpi-ws281x-powershell/issues/2
-                // The Console.WriteLine calls in WS281x looked like they were saving me
-                // but that appears to only work for the simple once-through run of the
-                // color wipe. When I try to do multiple things, fail.
-                /*
                 Console.WriteLine("Beginning test - press any key to exit.");
                 var tokenSource = new CancellationTokenSource();
                 var task = Task.Run(
@@ -55,11 +46,6 @@ namespace ConsoleDemo
                 Console.WriteLine("Finishing up.");
                 tokenSource.Cancel();
                 task.Wait();
-                */
-
-                // Simple for now - just wipe red, then clear the lights.
-                Console.WriteLine(animations[0].ToString());
-                animations[0].Execute(CancellationToken.None);
 
                 var off = new ColorWipe(controller, Color.Black);
                 off.Execute(CancellationToken.None);
