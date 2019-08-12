08a9191130b5f93dc7dc3fbbd55ba0a6eab808fc
Travis Illig <tillig@paraesthesia.com>
2019-08-12 10:54:15 -0700

    Power toggle works.

--- src/PiLights/wwwroot/js/site.js
+++ src/PiLights/wwwroot/js/site.js
@@ -1,10 +1,27 @@
 ﻿function pollSmartPlugStatus() {
+    var powerIsOn = false;
     (function p() {
         $.ajax({
             url: "/power/status",
             dataType: "json"
         }).done(function (data) {
-            console.log("Light on? " + data.on);
+            if (data.on) {
+                if (!powerIsOn) {
+                    console.log("Power turned on.");
+                    powerIsOn = true;
+                }
+                $("#powerForm").attr("action", "/power/shutdown");
+                $('.power-off-button').show();
+                $('.power-on-button').hide();
+            } else {
+                if (powerIsOn) {
+                    console.log("Power turned off.");
+                    powerIsOn = true;
+                }
+                $("#powerForm").attr("action", "/power/startup");
+                $('.power-on-button').show();
+                $('.power-off-button').hide();
+            }
         }).fail(function (xhr, status, err) {
             console.log("Error: " + err);
         });
@@ -13,5 +30,18 @@
 }
 
 $(function () {
+    $('#powerForm').submit(function (e) {
+        e.preventDefault();
+        $.ajax({
+            url: $("#powerForm").attr("action"),
+            type: 'post',
+            data: $('#powerForm').serialize(),
+            success: function () {
+                $('#powerOnModal').modal('hide');
+                $('#powerOffModal').modal('hide');
+            }
+        });
+    });
+
     pollSmartPlugStatus();
 });
